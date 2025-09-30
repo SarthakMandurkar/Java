2e8547ecdb89895296f6b7e707ba6f4edd006e1d
SarthakMandurkar <sarthakmandurkar17@gmail.com>
2025-09-30 15:37:46 +0530

    Area of Circle.

---
+++ DSA-Bootcamp-Java/src/Basics/AreaOfCircle.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the radius of circle : ");
+        double radius = in.nextDouble();
+
+        double area = 3.14 * radius * radius;
+
+        System.out.println("Area of circle : " + area);
+    }
+}
