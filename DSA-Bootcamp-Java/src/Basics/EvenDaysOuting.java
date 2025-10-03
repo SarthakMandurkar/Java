ef5d83f257da2ba8b916ba72ea15589f0efd7202
SarthakMandurkar <sarthakmandurkar17@gmail.com>
2025-10-04 00:05:14 +0530

    Going out on even days.

---
+++ DSA-Bootcamp-Java/src/Basics/EvenDaysOuting.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class EvenDaysOuting {
+    public static void main(String[] args) {
+
+        int count = 0;
+        int noOfDays = 31;
+
+        for(int i = 1; i <= noOfDays; i++){
+            if(i % 2 == 0){
+                count++;
+            }
+        }
+        System.out.println("The number of days Kunal can go out is : " + count);
+    }
+}
